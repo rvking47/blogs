dc63fd9224192cd448819356f484579fd8affd32
Rahul Vimal <rviml358@gmail.com>
2025-06-29 12:13:35 +0530

    agine update

--- database/db.js
+++ database/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try{
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log("Connected to Database!!");
+    const conn= await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+   console.log(`MongoBD Connected: ${conn.connection.host}`);
     }
     catch(error)
     {
